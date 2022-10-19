13ae78c965beb27f86b88eab1728cca828fede16
Brian Vermeer <brianvermeer@snyk.io>
2022-10-19 13:22:39 -0700

    typo fix

--- src/main/java/io/snyk/demo/AuthenticationFailureListener.java
+++ src/main/java/io/snyk/demo/AuthenticationFailureListener.java
@@ -19,6 +19,6 @@ public class AuthenticationFailureListener implements ApplicationListener<Authen
 
     @Override
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
-        logger.error("Failed login attemt for username:" + e.getAuthentication().getName());
+        logger.error("Failed login attempt for username:" + e.getAuthentication().getName());
     }
 }
